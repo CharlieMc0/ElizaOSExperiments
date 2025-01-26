e1807e0bbbdff118a1e39323f4cc239e2c1377d2
CharlieM <31941002+CharlieMc0@users.noreply.github.com>
2025-01-25 20:07:34 -0600

    experiments

---
+++ scripts/reset.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Navigate to the script's directory
+cd "$(dirname "$0")"/..
+echo "Cleanup started."
+# Find and remove node_modules directories, dist directories.
+find . -type d -name "node_modules" -exec rm -rf {} + \
+    -o -type d -name "dist" -exec rm -rf {} + \
+    -o -type d -name ".turbo" -exec rm -rf {} +
+
+# Remove core cache
+rm -rf ./packages/core/cache
+
+rm -rf ./node_modules
+# Remove pnpm lockfile
+rm ./pnpm-lock.yaml
+
+echo "Cleanup completed."
+
+pnpm install --no-frozen-lockfile
+echo "Installation completed."
+
+pnpm build
+echo "Build completed."
